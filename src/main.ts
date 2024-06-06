c752baba126f52447316cd439f0f46f85126d3e1
张庭兴 <1517607397@qq.com>
2024-06-06 14:49:05 +0800

    feat: 显示 glb 格式的飞机模型

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import './style.css'
-import {Cesium3DTileset, Viewer} from "cesium";
+import {Cartesian3, HeadingPitchRoll, Math, Transforms, Viewer} from "cesium";
+import Air from './assets/Cesium_Air.glb?url'
 
-const viewer = new Viewer('app');
-
-
-const tileset = viewer.scene.primitives.add(
-    await Cesium3DTileset.fromIonAssetId(2464651),
+const viewer = new Viewer('app', {
+    shouldAnimate: true, // 开启动画
+});
+// 飞机位置
+const position = Cartesian3.fromDegrees(
+    117.36237,
+    32.934037,
+    5000
 );
+// 飞机姿态
+const heading = Math.toRadians(135);
+const pitch = 0.4;
+const roll = -0.3;
+const hpr = new HeadingPitchRoll(heading, pitch, roll);
+const orientation = Transforms.headingPitchRollQuaternion(
+    position,
+    hpr
+);
+// 显示出来
+const entity = viewer.entities.add({
+    name: Air,
+    position: position,
+    orientation: orientation,
+    model: {
+        uri: Air,
+        minimumPixelSize: 128,
+        maximumScale: 20000,
+    },
+});
 
-await viewer.zoomTo(tileset)
+await viewer.zoomTo(entity)
+
+viewer.trackedEntity = entity;
